05256b909f390dcf90c11a09851adbe30043474e
WeslleyMouraDev <weslleymouradev@gmail.com>
2023-06-10 20:53:58 -0300

    feat: Realiza mudanças nos componentes Board-s e trabalha a interação e criação dos quadros e colunas para as tarefas

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,22 +1,45 @@
 "use client";
 import { useBoardStore } from "@/store/BoardStore";
 import { useEffect } from "react";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 
 function Board() {
-  const getBoard = useBoardStore((state) => state.getBoard);
+  const [board, getBoard] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+  ]);
 
   useEffect(() => {
     getBoard();
   }, [getBoard]);
 
+  const handleOnDragEnd = (result: DropResult) => {
+
+  }
+
   return (
-    <h1>Hello</h1>
-    // <DragDropContext>
-    //   <Droppable droppableId="board" direction="horizontal" type="column">
-    //     {(provided) => <div>renderiza todas as colunas</div>}
-    //   </Droppable>
-    // </DragDropContext>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId="board" direction="horizontal" type="column">
+        {(provided) =>
+            <div
+            className="grid grid-cols-1 md:grid-cols-2 gap-5 max-w-7xl mx-auto"
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+            >{
+                Array.from(board.columns.entries()).map(([id, column], index) => (
+                    <Column 
+                        key={id}
+                        id={id}
+                        tasks={column.tasks}
+                        index={index}
+
+                    />
+                ))
+                }</div>
+            
+            }
+      </Droppable>
+    </DragDropContext>
   );
 }
 
--- lib/getTasksGroupedByColumn.ts
+++ lib/getTasksGroupedByColumn.ts
@@ -7,4 +7,47 @@ export const getTasksGroupedByColumn = async () => {
   );
 
   const tasks = data.documents;
+
+  const columns = tasks.reduce((acc, task) => {
+    if (!acc.get(task.status)) {
+      acc.set(task.status, {
+        id: task.status,
+        tasks: [],
+      });
+    }
+
+    acc.get(task.status)!.tasks.push({
+      $id: task.$id,
+      $createdAt: task.$createdAt,
+      title: task.title,
+      status: task.status,
+      // captura a imagem caso exista na tarefa
+      ...(task.image && { image: JSON.parse(task.image) }),
+    });
+
+    return acc;
+  }, new Map<TypedColumn, Column>());
+
+  // Caso não exista nenhuma tarefa a fazer, em progresso ou concluída, mostra o qudro vazio
+  const columnTypes: TypedColumn[] = ["task", "inprogress", "done"];
+  for (const columnType of columnTypes) {
+    if (!columns.get(columnType)) {
+      columns.set(columnType, {
+        id: columnType,
+        tasks: [],
+      });
+    }
+  }
+
+  const sortedColumns = new Map(
+    Array.from(columns.entries()).sort(
+      (a, b) => columnTypes.indexOf(a[0]) - columnTypes.indexOf(b[0])
+    )
+  );
+
+  const board: Board = {
+    columns: sortedColumns,
+  };
+
+  return board;
 };
